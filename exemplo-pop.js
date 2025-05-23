0d3a2f78c0c177540fc1fb8e123ca74933844ef4
Jhonatan <jhonatan.favoreto18@gmail.com>
2025-05-23 08:33:01 -0300

    Adiciona arquivos ARRAY.

---
+++ exemplo-pop.js
@@ -0,0 +1,8 @@
+// pop() - Remove o último elemento do array
+let playlist = ['Funk', 'Hip-Hop', 'Pop'];
+console.log("Antes do push:", playlist);
+
+// Removendo a última música da playlist
+let musicaRemovida = playlist.pop();
+console.log("Depois do pop:", playlist);
+console.log("Música removida:", musicaRemovida);
---
+++ exemplo-push.js
@@ -0,0 +1,7 @@
+// push() - Adiciona elementos ao final do array
+let playlist = ['Funk', 'Hip-Hop', 'Pop'];
+console.log("Antes do push:", playlist);
+
+// Adicionando uma nova música á playlist
+playlist.push('Trap');
+console.log("Depois do push:", playlist);
